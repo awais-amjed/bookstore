3f497a8157317218e47e3bf9c48922fc9a8cc55e
Awais Amjed <awaisamjed171@gmail.com>
2022-03-24 19:06:54 +0500

    Add Book Component

--- src/components/pages/books_page.js
+++ src/components/pages/books_page.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import Navbar from '../model/navbar';
+import Book from '../model/book';
 
-const BooksPage = () => (
-  <div className="content">
-    <div id="books-page">
-      <Navbar />
+const BooksPage = () => {
+  const booksList = [
+    {
+      id: 0,
+      category: 'Action',
+      name: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      progress: 64,
+      currentChapter: 17,
+      chapterName: null,
+    },
+    {
+      id: 1,
+      category: 'Science Fiction',
+      name: 'Dune',
+      author: 'Frank Herbert',
+      progress: 8,
+      currentChapter: 3,
+      chapterName: 'A Lesson Learned',
+    },
+    {
+      id: 2,
+      category: 'Economy',
+      name: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+      progress: 0,
+      currentChapter: null,
+      chapterName: 'Introduction',
+    },
+  ];
+
+  return (
+    <div className="content">
+      <div id="books-page">
+        <Navbar />
+        {booksList.map(({
+          id,
+          category,
+          name,
+          author,
+          progress,
+          currentChapter,
+          chapterName,
+        }) => (
+          <Book
+            key={id}
+            category={category}
+            name={name}
+            author={author}
+            progress={progress}
+            chapterName={chapterName}
+            currentChapter={currentChapter}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default BooksPage;
