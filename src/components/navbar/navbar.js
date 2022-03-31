1530a944c044ea817b459675ffc32002fb631e31
Awais Amjed <awaisamjed171@gmail.com>
2022-03-31 13:30:21 +0500

    Add Styling to Navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import './navbar.scss';
 import { NavLink } from 'react-router-dom';
+import userImage from '../images/user.png';
 
 const Navbar = () => (
   <header>
-    <h1>Bookstore CMS</h1>
-    <nav id="navbar">
-      <NavLink className="link-button" to="/">
-        BOOKS
-      </NavLink>
-      &nbsp;
-      <NavLink className="link-button" to="/categories">
-        CATEGORIES
-      </NavLink>
-    </nav>
+    <div className="nav-left">
+      <h1>Bookstore CMS</h1>
+      <nav id="navbar">
+        <NavLink className="link-button" to="/">
+          BOOKS
+        </NavLink>
+        <NavLink className="link-button" to="/categories">
+          CATEGORIES
+        </NavLink>
+      </nav>
+    </div>
+    <img src={userImage} height="30px" alt="User Icon" />
   </header>
 );
 
